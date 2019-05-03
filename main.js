217bda98306983603bb954f8d6209a811e361729
varshakhandre14 <varshakhandre14@gmail.com>
2019-05-03 14:15:18 +0800

    Latest Image frontend code upload

--- main.js
+++ main.js
@@ -6,14 +6,6 @@ var user = getUserInfo();
 
 var allImages = "";
 
-/*
-for (var i = 0; i < 298; i++) {
-  var width = getRandomSize(200, 400);
-  var height =  getRandomSize(200, 400);
-  
-  allImages += '<img class="myImg" src="http://lorempixel.com/'+width+'/'+height+'/sports" id='+i+'_id alt="pretty kitty" onclick="openpopup(this.src)">';
-  
-}*/
 var totalinrow = 20;
 var row = 1;
 var col = 1;
@@ -24,35 +16,53 @@ var imginfo = imgurl[1];
 
 imginfo = sortByKey(imginfo, 'sort');
 
-//console.log(imginfo);
 
-for (var i = 0, j = imginfo.length; i < j; i++) {
+console.log(imginfo);
+
+for (var i = 1, j = imginfo.length; i < j; i++) {
     
-    if(imgurl[0][i] == undefined){imgurl[0][i]="image/upload.png"}
+    //if(imgurl[0][i] == undefined){imgurl[0][i]="image/upload.png"}
+    if(imginfo[i]["url"] == undefined){imginfo[i]["url"]="image/upload.png"}
     
-    if(typeof imginfo[i] === "undefined"){image_info = ""; name = ""; }
+    if(typeof imginfo[i] === "undefined"){image_info = ""; name = ""; introduction = ""; }
     else{
-        image_info = "owner=" + imginfo[i]["owner"] + " and price=" + imginfo[i]["price"];
+        image_info = imginfo[i]["owner"] + "," + imginfo[i]["price"]+ "," +String(imginfo[i]["introduction"]).trim();
         introduction = String(imginfo[i]["introduction"]).trim();
     }
-    //allImages += '<img class="myImg" src='+ imgurl[0][i] +' id='+i+'_id onclick="openpopup(this.src,\''+image_info+'\');" title="'+introduction+'" alt="">';
-    allImages += '<div class="blog" id=' + row + '_'+col+'_div><img class="myImg" src='+ imgurl[0][i] +' id='+i+'_id onclick="openpopup(this.src,\''+image_info+'\');" title="'+introduction+'" alt=""></div>';
 
+    var coordinateno = i;
+    //allImages += '<img class="myImg" src='+ imgurl[0][i] +' id='+i+'_id onclick="openpopup(this.src,\''+image_info+'\');" title="'+introduction+'" alt="">';
+    allImages += '<div class="blog" id=' + row + '_'+col+'_div><img class="myImg" src='+ imginfo[i]["url"] +' id='+coordinateno+' onclick="openpopup(this.id,this.src,\''+image_info+'\');" title="'+introduction+'" alt=""></div>';
+    
 }
-//$('#photos').append(allImages);
+
 $('#blogPosts').append(allImages);
+
+$('[id="inputurl"]').on('change', function() {
+  $('#img02').attr('src', this.value);
+  CheckImagedataempty();
+});
+
+$('[id="txtintro"]').on('change', function() {
+  CheckImagedataempty();
+});
 
 };
 
 //////////////////Open ModalPopup/////////////////
-function openpopup(src,info){
+function openpopup(id,src,info){
     var modal = document.getElementById('myModal');
-    var modalImg = document.getElementById("img01");
+    var modalImg = document.getElementsByName("img01")[0];
     var captionText = document.getElementById("caption");
+    
 
     modal.style.display = "block";
     modalImg.src = src;
-    captionText.innerHTML = info;
+    modalImg.id = id;
+    var imginfoarray = info.split(",");
+    document.getElementById("span_owner").innerHTML = imginfoarray[0].trim();
+    document.getElementById("span_price").innerHTML = imginfoarray[1].trim();
+    captionText.innerHTML = imginfoarray[2].trim();
 
 }
 
@@ -60,23 +70,27 @@ function openpopup(src,info){
 function closepopup(){
     var modal = document.getElementById('myModal');
     modal.style.display = "none";
+    document.getElementById("inputurl").value="";
+    $('#img02').attr('src', null);
+    document.getElementById("txtintro").value="";
+    document.getElementById('span_inputhex').innerHTML="";
 }
 
 
-////// Getimages from api ///////////////////////////
+////// Get images information from api ///////////////////////////
 function getImages()
 {
   var imagearr = [];
   var imageinfo = [];
   
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://3.112.106.186:7744/casigo/sDAGpixel', false);
+  request.open('GET', 'http://192.168.51.212:3368/casigo/sDAGpixel?limit=200', false);
   request.onload = function () {
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     for(var j=0;j<data.length;j++) {
       imagearr.push(data[j].url);
-      imageinfo.push({"owner":data[j].owner,"price":data[j].price, "introduction":data[j].introduction, "sort":data[j].sort});
+      imageinfo.push({"url":data[j].url,"owner":data[j].owner,"price":data[j].price, "introduction":data[j].introduction, "sort":data[j].sort});
     }
   } else {
     console.log('error');
@@ -103,8 +117,7 @@ function getUserInfo()
   var a = [];
   
   var request = new XMLHttpRequest();
-  //request.open('GET', 'http://192.168.51.212:3368/casigo/sDAGpixel', false);
-  request.open('GET', 'http://3.112.106.186:7744/casigo/sDAGinfo', false);
+  request.open('GET', 'http://192.168.51.212:3368/casigo/sDAGinfo', false);
   request.onload = function () {
   var data = JSON.parse(this.response);
   console.log(data);
@@ -125,9 +138,35 @@ function getUserInfo()
   }
   // Send request
   request.send(null);
-
-  return a;
 }
 
 
+///Send image url, coordinate and introduction to get input hex
+function sendAPIData(url,introduction,coordinateid){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.response);
+        console.log(this.responseText);
+        document.getElementById("span_inputhex").innerHTML = data[0].input;
+      }
+    };
+    xhttp.open("POST", "http://192.168.51.212:3368/casigo/sDAGinput",true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    var input = JSON.stringify({
+      "coordinate": coordinateid,
+      "url": url,
+      "introduction": introduction
+    });
+    xhttp.send(input);
+}
 
+///check if user inputs url and introduction both
+function CheckImagedataempty() {
+  var url = document.getElementById('inputurl').value;
+  var introduction = document.getElementById('txtintro').value;
+  var coordinateid = document.getElementsByName('img01')[0].id;
+  if (( url != '') && (introduction != '')) {
+    sendAPIData(url,introduction,coordinateid);
+  }
+}
