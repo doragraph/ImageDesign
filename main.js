6a4f89c3cd18bca32157fa27c700d908dcb75cb9
varshakhandre14 <varshakhandre14@gmail.com>
2019-05-07 15:38:27 +0800

    Connect to metamask

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var ipaddress = '13.75.117.140';
+//var ipaddress = '192.168.51.212';
 var port = '3368';
 
 
@@ -20,7 +21,7 @@ var imginfo = imgurl[1];
 imginfo = sortByKey(imginfo, 'sort');
 
 
-console.log(imginfo);
+//console.log(imginfo);
 
 for (var i = 1, j = imginfo.length; i < j; i++) {
     
@@ -38,6 +39,7 @@ for (var i = 1, j = imginfo.length; i < j; i++) {
     allImages += '<div class="blog" id=' + row + '_'+col+'_div><img class="myImg" src='+ imginfo[i]["url"] +' id='+coordinateno+' onclick="openpopup(this.id,this.src,\''+image_info+'\');" title="'+introduction+'" alt="" onerror="this.onerror=null;this.src=\'https://media1.giphy.com/media/j5QUSpXVuwtr2/giphy.gif?cid=790b76115ccc211b71584e665985d92f&rid=giphy.gif\';"></div>';
     
 }
+
 
 $('#blogPosts').append(allImages);
 
@@ -57,14 +59,20 @@ function openpopup(id,src,info){
     var modal = document.getElementById('myModal');
     var modalImg = document.getElementsByName("img01")[0];
     var captionText = document.getElementById("caption");
-    
 
     modal.style.display = "block";
     modalImg.src = src;
     modalImg.id = id;
     var imginfoarray = info.split(",");
+
+    var weiprice = imginfoarray[1].trim();
+    var etherprice = weiprice / 1000000000000000000;
+    console.log(etherprice);
+
+    document.getElementById('weiprice').value = weiprice;
+
     document.getElementById("span_owner").innerHTML = imginfoarray[0].trim();
-    document.getElementById("span_price").innerHTML = imginfoarray[1].trim();
+    document.getElementById("span_price").innerHTML = etherprice;
     captionText.innerHTML = imginfoarray[2].trim();
     document.getElementById("span_address").innerHTML = imginfoarray[3].trim();
 
@@ -81,6 +89,13 @@ function closepopup(){
     document.getElementById('span_owner').innerHTML="";
     document.getElementById('span_price').innerHTML="";
     document.getElementById('span_address').innerHTML="";
+
+    $('#btn_buy_meta').prop('disabled', true);
+    $('#btn_buy_myether').prop('disabled', true);
+    document.getElementById("weiprice").value="";
+
+    document.getElementById('span_metalink').innerText ="";
+    document.getElementById('span_metalink').href ="#";
 }
 
 
@@ -126,18 +141,18 @@ function getUserInfo()
   
   var request = new XMLHttpRequest();
   request.open('GET', 'http://'+ipaddress+':'+port+'/casigo/sDAGinfo', false);
- // request.open('GET', 'http://localhost:3368/casigo/sDAGinfo', false);
+  //request.open('GET', 'http://192.168.51.212:3368/casigo/sDAGinfo', false);
   request.onload = function () {
   var data = JSON.parse(this.response);
-  console.log(data);
+  //console.log(data);
   if (request.status >= 200 && request.status < 400) {
 
-    var objectNames = Object.keys(data);
-    var th_users = objectNames[0];
-    var th_transaction = objectNames[3];
+    //var objectNames = Object.keys(data);
+    //var th_users = objectNames[2];
+    //var th_transaction = objectNames[1];
 
-    document.getElementById("24h_transaction").innerHTML = data[th_transaction];
-    document.getElementById("24h_users").innerHTML = data[th_users];
+    document.getElementById("24h_transaction").innerHTML = data.TFHtransaction;
+    document.getElementById("24h_users").innerHTML = data.TFHuser;
     document.getElementById("sc_bal").innerHTML = data.contactBalance;
     document.getElementById("total_transaction").innerHTML = data.totalTransaction;
     document.getElementById("total_users").innerHTML = data.totaluser;
@@ -158,6 +173,8 @@ function sendAPIData(url,introduction,coordinateid){
         var data = JSON.parse(this.response);
         console.log(this.responseText);
         document.getElementById("span_inputhex").innerHTML = data[0].input;
+        $('#btn_buy_meta').prop('disabled', false);
+        $('#btn_buy_myether').prop('disabled', false);
       }
     };
     xhttp.open("POST", "http://"+ipaddress+":"+port+"/casigo/sDAGinput",true);
@@ -177,5 +194,50 @@ function CheckImagedataempty() {
   var coordinateid = document.getElementsByName('img01')[0].id;
   if (( url != '') && (introduction != '')) {
     sendAPIData(url,introduction,coordinateid);
+  }
+}
+
+
+async function connectMetamask(){
+
+  //Collect param values
+
+  var wei = Number(document.getElementById('weiprice').value);
+  var weitohex = "0x" + wei.toString(16);
+  var inputhex = document.getElementById('span_inputhex').innerHTML.trim();
+  var smartContractaddress = document.getElementById('span_address').innerHTML.trim();
+
+  console.log(weitohex+','+inputhex+','+smartContractaddress)
+
+  if (typeof window.ethereum !== 'undefined'
+      || (typeof window.web3 !== 'undefined')) {
+      // Web3 browser user detected. You can now use the provider.
+      const provider = window['ethereum'] || window.web3.currentProvider
+  }
+  console.log(ethereum.isMetaMask);
+
+  try {
+    const accounts = await ethereum.enable();
+    console.log("account: " +  accounts[0]);
+
+    ethereum.send({
+      method: 'eth_sendTransaction',
+      params: [{"from": accounts[0],
+      "to": smartContractaddress,
+      "gas": "0x2DC6C0", // 30400
+      "gasPrice": "0x2540BE400", 
+      "value": weitohex, // 2441406250
+      "data": inputhex}],
+      //from: web3.eth.accounts[0], // Provide the user's account to use.
+      from: accounts[0],
+    },function(err, transactionHash) {
+      if (!err)
+        console.log(transactionHash); 
+        document.getElementById('span_metalink').innerText="https://kovan.etherscan.io/tx/"+transactionHash.result;
+        document.getElementById('span_metalink').href="https://kovan.etherscan.io/tx/"+transactionHash.result;
+    })
+
+  } catch (error) {
+    console.log(error === "User rejected provider access")
   }
 }
